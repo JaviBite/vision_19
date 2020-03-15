b5f27badb8f9b32b3d27629109944205e125145c
738866 <738866@unizar.es>
2020-03-15 14:35:25 +0100

    Points 1 to 3)

--- workspace/T2/src/main.cpp
+++ workspace/T2/src/main.cpp
@@ -5,6 +5,7 @@
 #include "utils.hpp"
 #include <fstream>
 #include <string>
+#include <vector>
 #define CV_WINDOW_AUTOSIZE WINDOW_AUTOSIZE
 #define CV_TERMCRIT_ITER 1
 
@@ -13,391 +14,66 @@ using namespace cv;
 using namespace std;
 
 
-int ct = 0;
-char tipka;
-char filename[100]; // For filename
-int  c = 1; // For filename
+int main(int, char**) {
 
-int main(int, char**)
-{
-	// VARIABLES
-	double contrast = 1;
-	int numeroColores = 8;
-	int alien_mode = 0;
-	int dist_mode = 0;
-	int take_mode = 2;
-	int gauss_k = 1;
-	int gauss_s = 5;
-	int pixels = 64;
-	int interpolation = INTER_NEAREST;
-	float dist_k = 0.5;
+	Mat image;
+	image = imread("files/imagenesT2/reco1.pgm", CV_LOAD_IMAGE_COLOR);   // Read the file
+	checkImg(image);
 
-    int histSize = 256; //from 0 to 255
+	imshow("Display window", image );                   // Show our image inside it.
+	waitKey(0);
+	cvDestroyWindow("Display window");
 
+	//Point 1: Get Binary Images
 
-	bool contraste = false;
-	bool reduccionColores = false;
-	bool efectoAlien = false;
-	bool distorsion = false;
-	bool take_effect = false;
-	bool hist_eq = false;
-	bool hist_eq_ours = false;
-	bool gauss = false;
-	bool negative_effect = false;
-	bool gray_scale = false;
-	bool pixel = false;
-	bool k_means = false;
+	Mat bin_otsu, bin_adap;
 
-	bool uniform = true;
-	bool accumulate = false;
-	bool accumulate_hist = false;
+	double maxval = 255;
+	cv::max(image, maxval);
+	int type = THRESH_BINARY_INV;
 
-    Mat frame;
-    //--- INITIALIZE VIDEOCAPTURE
-    VideoCapture cap;
-    // open the default camera using default API
-    cap.open(0);
-    // OR advance usage: select any API backend
-    int deviceID = 0;             // 0 = open default camera
-    int apiID = cv::CAP_ANY;      // 0 = autodetect default API
-                                  // open selected camera using selected API
-    cap.open(deviceID + apiID);
-    // check if we succeeded
-    if (!cap.isOpened()) {
-        cerr << "ERROR! Unable to open camera\n";
-        return -1;
-    }
-    //--- GRAB AND WRITE LOOP
-    cout << "Start grabbing" << endl
-         << "Press q to terminate" << endl
-    << "Press a to alternate accumulated/normal histogram" << endl
-    << "Press s to alternate max of histogram (250/256)" << endl
-    << "Press g to switch gaussian blur" << endl
-	<< "Press p to switch pixel mode (press +/- modify num of pixels and press * to switch interpolations" << endl
-    << "EFFECTS:\n-------------" << endl
-    << "0 to reset effects" << endl
-    << "1 contrast (press z to increase)" << endl
-    << "2 color reduction (press x to increase, press k for kmeans)" << endl
-    << "3 alien (press c to change colour)" << endl
-    << "4 distortion (press v to switch dist, and ,/. modify k)" << endl
-    << "5 take on me (press b to increase threshold)" << endl
-    << "6 equalization openCV " << endl
-    << "7 equalization ours" << endl
-    << "8 negative" << endl
-	<< "9 gray scale" << endl
-	<< "COOL COMBINATIONS\n---------------" << endl
-	<< "8 + 5 + b + g" << endl
-	<< "4 + v + v + 9 + p + *" << endl
-    << "p + '+'7 + 2 + x5" << endl;
+	bin_otsu = toBinaryOtsu(image, maxval, type);
 
-    for (;;)
-    {
-        // wait for a new frame from camera and store it into 'frame'
-        cap.read(frame);
+	int adap_meth = ADAPTIVE_THRESH_MEAN_C;
+	int blocksize = 11;
+	int C = 2;
+	bin_adap = toBinaryAdapt(image,  maxval, type, adap_meth, blocksize, C);
 
-        if (frame.empty()) {
-            cerr << "ERROR! blank frame grabbed\n";
-            break;
-        }
+	imshow("OTSU", bin_otsu );                   // Show our image inside it.
+	imshow("ADAPTIVE", bin_adap );
+	waitKey(0);
+	cvDestroyWindow("ADAPTIVE");
+	cvDestroyWindow("OTSU");
 
-        Sleep(5); // Sleep is mandatory - for no leg!
+	//Point 2: Contours
 
-        //apply filter
-        //Mat I = apply_effect_rgb(frame, alien_blue, 50);
-        //Mat I = apply_effect(frame, take_on_me, 10);
+	image = imread("files/imagenesT2/circulo1.pgm", CV_LOAD_IMAGE_COLOR);   // Read the file
+	checkImg(image);
 
-        //EFECTOS
-        if (gauss) {
-        	GaussianBlur(frame, frame, Size2i(gauss_s,gauss_s), gauss_k, gauss_k, BORDER_DEFAULT);
-        }
-        if (gray_scale) {
-        	cvtColor(frame, frame, CV_RGB2GRAY);
-        }
-		if (contraste) {
+	imshow("Display window", image );                   // Show our image inside it.
+	waitKey(0);
 
-			Mat ycrcb;
+	image = toBinaryOtsu(image);
 
-				cvtColor(frame,ycrcb,CV_BGR2HSV);
+	Mat draw_contours = Mat::zeros( image.size(), CV_8UC3 );
+	std::vector<std::vector<Point>> contours;
+	std::vector<Vec4i> hierarchy;
 
-				vector<Mat> channels;
-				split(ycrcb,channels);
+	int mode = CV_RETR_TREE;
+	int method = CV_CHAIN_APPROX_NONE;
+	RNG rng(12345);
 
-				channels[2] = apply_effect(channels[2], contrastF, contrast);
-				merge(channels,ycrcb);
+	cv::findContours(image, contours, hierarchy, mode, method);
+	//cv::drawContours(draw_contours, contours, -1, cv::COLORMAP_JET);
+	for( int i = 0; i< contorus.size(); i++ ) {
+		Scalar color = Scalar( rng.uniform(0, 255), rng.uniform(0,255), rng.uniform(0,255) );
+		cv::drawContours( draw_contours, contours, i, color, 2, 8, hierarchy, 0, Point() );
+	}
 
-				cvtColor(ycrcb,frame,CV_HSV2BGR);
-
-			//frame = apply_effect(frame, contrastF, contrast);
-			putText(frame,to_string(contrast),Point2f(16,20),FONT_HERSHEY_PLAIN, 1,  Scalar(0,0,255), 2 , 8 , false);
-		}
-		if (hist_eq) frame = equalizarCV(frame);
-		if (hist_eq_ours) frame = equalizarOurs(frame);
-		if (reduccionColores) {
-			if (k_means && !gray_scale) {
-				Mat data;
-				frame.convertTo(data,CV_32F);
-				data = data.reshape(1,data.total());
-
-				Mat labels, centers;
-				kmeans(data, 8, labels, TermCriteria(CV_TERMCRIT_ITER, 10, 1.0), 3,
-				       KMEANS_PP_CENTERS, centers);
-
-				centers = centers.reshape(3,centers.rows);
-				data = data.reshape(3,data.rows);
+	imshow("Contours", draw_contours);                   // Show our image inside it.
+	waitKey(0);
+	cvDestroyWindow("Contours");
 
 
-				Vec3f *p = data.ptr<Vec3f>();
-				for (size_t i=  0; (int)i < data.rows; i++) {
-				   int center_id = labels.at<int>(i);
-				   p[i] = centers.at<Vec3f>(center_id);
-				}
-
-
-				frame = data.reshape(3, frame.rows);
-				frame.convertTo(frame, CV_8U);
-			}
-			else {
-				frame = apply_effect(frame, reducirColorF, 256 / cbrt(numeroColores));
-				putText(frame,to_string(numeroColores),Point2f(16,20),FONT_HERSHEY_PLAIN, 1,  Scalar(0,0,255), 2 , 8 , false);
-			}
-		}
-
-		if (efectoAlien) {
-			Mat skin = skinMat(frame);
-			//frame = skin;
-			frame = generarAlien(skin,frame, alien_mode);
-
-		}
-
-		if (take_effect) {
-			frame = apply_effect(frame, take_on_me, take_mode);
-			putText(frame,to_string(take_mode),Point2f(16,20),FONT_HERSHEY_PLAIN, 1,  Scalar(0,0,255), 2 , 8 , false);
-		}
-		if (negative_effect) {
-			frame = apply_effect(frame, negative, 0);
-		}
-		if (pixel) {
-			Mat temp;
-			resize(frame, temp, Size2i(pixels, pixels), 0, 0, INTER_LINEAR);
-			resize(temp, frame, Size2i(frame.cols, frame.rows), 0, 0, interpolation);
-		}
-		if (distorsion) generarDistorsion(frame, dist_mode, dist_k);
-
-        // show live and wait for a key with timeout long enough to show images
-        imshow("CAMERA 1", frame);  // Window name
-
-
-        tipka = cv::waitKey(30);
-
-        switch (tipka) {
-        case '1':
-        	contraste = !contraste;
-        	break;
-        case '2':
-            reduccionColores = !reduccionColores;
-            break;
-        case '3':
-        	efectoAlien = !efectoAlien;
-        	if (efectoAlien) gray_scale = false;
-            break;
-        case '4':
-        	distorsion = !distorsion;
-            break;
-        case '5':
-        	take_effect = !take_effect;
-        	break;
-        case '6':
-			hist_eq = !hist_eq;
-			if (hist_eq) gray_scale = false;
-			break;
-        case '7':
-			hist_eq_ours = !hist_eq_ours;
-			if (hist_eq_ours) gray_scale = false;
-			break;
-        case '8':
-			negative_effect = !negative_effect;
-			break;
-        case '9':
-			gray_scale = !gray_scale;
-			if (gray_scale) {
-				efectoAlien = false;
-				hist_eq_ours = false;
-				hist_eq = false;
-			}
-
-			break;
-        case 'p':
-        	pixel = !pixel;
-        	break;
-        case 'g':
-			gauss = !gauss;
-			break;
-
-
-
-
-        case 'v':
-				dist_mode = (dist_mode + 1) % 2;
-				break;
-        case 'c':
-				alien_mode = (alien_mode + 1) % 3;
-				break;
-        case 'z':
-				contrast = contrast + 0.2;
-				if (contrast > 4) contrast = 0;
-				break;
-        case 'x':
-				//numeroColores = (numeroColores + 1) % 64;
-        		numeroColores = pow((cbrt(numeroColores) + 1),3);
-        		if (numeroColores > 1331) numeroColores = 8;
-				break;
-        case 'b':
-				take_mode = (take_mode + 2) % 20 + 2;
-				break;
-
-
-        case '\'':
-				gauss_k += 1;
-				break;
-        case '¡':
-				gauss_k -= 1;
-				break;
-        case '?':
-				gauss_s += 1;
-				break;
-		case '¿':
-				gauss_s -= 1;
-				break;
-		case '+':
-				pixels = pixels + 10;
-				break;
-		case '-':
-				pixels = pixels - 10;
-				if (pixels < 1) pixels = 1;
-				break;
-		case '*':
-				if (interpolation == INTER_NEAREST) interpolation = INTER_LINEAR;
-				else interpolation = INTER_NEAREST;
-				break;
-		case 'k':
-				k_means = !k_means;
-				break;
-		case ',':
-				dist_k += 0.25;
-				break;
-		case '.':
-				dist_k -= 0.25;
-				if (dist_k < 0) dist_k = 0;
-				break;
-
-
-		case 's':
-			if (histSize >= 256 ) histSize = 250;
-			else histSize = 256;
-			break;
-
-
-
-        case '0':
-            contraste = false;
-            reduccionColores = false;
-            efectoAlien = false;
-            distorsion = false;
-            take_effect = false;
-            hist_eq = false;
-			hist_eq_ours = false;
-			gauss = false;
-			negative_effect = false;
-			gray_scale = false;
-			pixel = false;
-
-			contrast = 1;
-			numeroColores = 8;
-			alien_mode = 0;
-			dist_mode = 0;
-			take_mode = 2;
-			gauss_k = 1;
-			gauss_s = 5;
-			pixels = 16;
-        }
-
-        if (tipka == 'd') {
-
-            sprintf(filename, "C://Frame_%d.jpg", c); // select your folder - filename is "Frame_n"
-            ofstream f_out("C://Frame.jpg");
-            f_out << frame;
-            f_out.close();
-            cv::waitKey(10);
-
-            imshow("CAMERA 1", frame);
-            imwrite(filename, frame);
-            cout << "Frame_" << c << endl;
-            c++;
-        }
-
-        if (tipka == 'q') {
-            cout << "Terminating..." << endl;
-            Sleep(2000);
-            break;
-        }
-
-        if (tipka == 'a') {
-        	accumulate_hist = !accumulate_hist;
-		}
-
-        //Historiogram
-
-        Mat channels[3];
-        split(frame,channels);
-        float range[] = { 0, (float)histSize } ; //the upper boundary is exclusive
-        const float* histRange = { range };
-
-        Mat b_hist, g_hist, r_hist;
-
-        /// Compute the histograms:
-        calcHist( &channels[0], 1, 0, Mat(), b_hist, 1, &histSize, &histRange, uniform, accumulate );
-        calcHist( &channels[1], 1, 0, Mat(), g_hist, 1, &histSize, &histRange, uniform, accumulate );
-        calcHist( &channels[2], 1, 0, Mat(), r_hist, 1, &histSize, &histRange, uniform, accumulate );
-
-        // Draw the histograms for R, G and B
-        int hist_w = 512; int hist_h = 400;
-        int bin_w = cvRound( (double) hist_w/histSize );
-
-        Mat histImage( hist_h, hist_w, CV_8UC3, Scalar( 0,0,0) );
-
-        if (accumulate_hist) {
-        	for( int i = 1; i < histSize; i++ )
-			{
-				b_hist.at<float>(i) = b_hist.at<float>(i-1) + b_hist.at<float>(i);
-				g_hist.at<float>(i) = g_hist.at<float>(i-1) + g_hist.at<float>(i);
-				r_hist.at<float>(i) = b_hist.at<float>(i-1) + r_hist.at<float>(i);
-			}
-		}
-
-        /// Normalize the result to [ 0, histImage.rows ]
-        normalize(b_hist, b_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-        normalize(g_hist, g_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-        normalize(r_hist, r_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
-
-        /// Draw for each channel
-        for( int i = 1; i < histSize; i++ )
-        {
-
-            line( histImage, Point( bin_w*(i-1), hist_h - cvRound(b_hist.at<float>(i-1)) ) ,
-                             Point( bin_w*(i), hist_h - cvRound(b_hist.at<float>(i)) ),
-                             Scalar( 255, 0, 0), 2, 8, 0  );
-            line( histImage, Point( bin_w*(i-1), hist_h - cvRound(g_hist.at<float>(i-1)) ) ,
-                             Point( bin_w*(i), hist_h - cvRound(g_hist.at<float>(i)) ),
-                             Scalar( 0, 255, 0), 2, 8, 0  );
-            line( histImage, Point( bin_w*(i-1), hist_h - cvRound(r_hist.at<float>(i-1)) ) ,
-                             Point( bin_w*(i), hist_h - cvRound(r_hist.at<float>(i)) ),
-                             Scalar( 0, 0, 255), 2, 8, 0  );
-        }
-
-        namedWindow("calcHist Demo", CV_WINDOW_AUTOSIZE );
-        imshow("calcHist Demo", histImage );
-
-    }
-    // the camera will be deinitialized automatically in VideoCapture destructor
     return 0;
 }
--- workspace/T2/src/utils.hpp
+++ workspace/T2/src/utils.hpp
@@ -13,300 +13,90 @@ using namespace cv;
 using namespace std;
 
 
+int histSize = 256; //from 0 to 255
+bool uniform = true;
+bool accumulat = false;
+bool accumulate_hist = false;
 
-// Aux Functions
-
-const uchar average[3] = {197, 140, 133};
-const uchar pale[3] = {236, 188, 180};
-const uchar pale_tan[3] = {209, 163, 164};
-const uchar tanned[3] = {161, 102, 94};
-const uchar black[3] = {80, 51, 53};
-const uchar brown[3] = {89, 47, 42};
-
-int skin_cmp(const uchar skin_tone[3], uchar compare[3]) {
-	return abs((int)skin_tone[0] - (int)compare[0])
-			+ abs((int)skin_tone[1] - (int)compare[1])
-			+ abs((int)skin_tone[2] - (int)compare[2]);
-}
-
-//bool is_skin(float threshold, uchar r, uchar g, uchar b) {
-void isSkin (uchar& hue, uchar& saturation, uchar& value, float aux) {
-	//	uchar skin[3] = {r, g, b};
-	//	if ( skin_cmp(average, skin) < threshold ||
-	//			skin_cmp(pale, skin) < threshold ||
-	//			skin_cmp(pale_tan, skin) < threshold ||
-	//			skin_cmp(tanned, skin) < threshold ||
-	//			//skin_cmp(black, skin) < threshold ||
-	//			//skin_cmp(brown, skin) < threshold ||
-	//			false) return true;
-	//	else return false;
-	//if( hue > 0 && hue < 20 && saturation > 48 && value > 80 ) {
-	 if (hue > 10 && hue < 25 && saturation > 48 && saturation < 255 && value > 80 ) {
-		 hue = 155; saturation = 155; value = 155;
-	 }
-	 else {
-		 hue = 0; saturation = 0; value = 0;
-	 }
-}
-
-// Effects
-
-uchar negative(uchar& c, uchar* end, double aux[]) {
-	return 255 - c;
-}
-
-//void negative(uchar& b, uchar& g, uchar& r, int aux[]){
-//   r = 255 - r;
-//   g = 255 - g;
-//   b = 255 - b;
-//}
-
-void alienBlue(uchar& b, uchar& g, uchar& r){
-	   r = saturate_cast< uchar >(r * ( 1 - 0.5));
-	   g = saturate_cast< uchar >(g * ( 1 - 0.5));
-	   b = saturate_cast< uchar >(b * ( 1 + 0.5));
-
-}
-
-void alienRed(uchar& b, uchar& g, uchar& r){
-	   r = saturate_cast< uchar >(r * ( 1 + 0.5));
-	   g = saturate_cast< uchar >(g * ( 1 - 0.5));
-	   b = saturate_cast< uchar >(b * ( 1 - 0.5));
-
-}
-
-void alienGreen(uchar& b, uchar& g, uchar& r){
-	   r = saturate_cast< uchar >(r * ( 1 - 0.5));
-	   g = saturate_cast< uchar >(g * ( 1 + 0.5));
-	   b = saturate_cast< uchar >(b * ( 1 - 0.5));
-
-}
-
-uchar take_on_me(uchar &a, uchar* end, double aux[]){
-	int nCols = aux[1];
-	int nChannels = aux[2];
-	int threshold = aux[3];
-	uchar* main = &a;
-	uchar* next = &a + nChannels;
-	uchar* down = &a + (nChannels * nCols);
-	if ( (next < end && abs((int)*main - (int)*next) < threshold) ||
-		 (down < end && abs((int)*main - (int)*down) < threshold)    ) {
-			 return 255;
-		 }
-		 else return 1;
-}
-
-
-
-// Functions
-
-Mat apply_effect(Mat I, function<uchar (uchar&, uchar*, double[])> effect, double threshold) {
-	double nRows = I.rows;
-	double nChannels = I.channels();
-	double nCols = I.cols * nChannels;
-	double info[4] = {nRows, nCols, nChannels, threshold};
-	uchar* end = I.ptr(I.size);
-	uchar* p;
-	if (I.isContinuous()){
-		nCols *= nRows;
-		nRows = 1;
+void checkImg(Mat img) {
+	if(!img.data ) {                          // Check for invalid input
+		cout <<  "Could not open or find the image" << std::endl ;
+		exit(-1);
 	}
-	for( int i = 0; i < nRows; ++i)
-		{
-			p = I.ptr<uchar>(i);
-			for ( int j = 0; j < nCols; ++j)
-			{
-				p[j] = effect(p[j], end, info);
-			}
-		}
-	return I;
 }
 
-Mat apply_effect_rgb(Mat I, function<void (uchar&, uchar&, uchar&, float)> effect, float threshold) {
-	int nRows = I.rows;
-	int nChannels = I.channels();
-	int nCols = I.cols * nChannels;
-	uchar* p;
-	if (I.isContinuous()){
-		nCols *= nRows;
-		nRows = 1;
-	}
-	for( int i = 0; i < nRows; ++i)
-		{
-			p = I.ptr<uchar>(i);
-			for ( int j = 0; j < nCols; j = j + nChannels)
-			{
+Mat toBinaryOtsu(Mat &img, int maxval = 255, int type = THRESH_BINARY_INV) {
+	Mat ret;
+	cv::cvtColor(img, ret, CV_RGB2GRAY);
+	ret.convertTo(ret, CV_8UC1);
+	if (maxval <= 0) cv::max(img, maxval);
 
-				effect(p[j], p[j+1], p[j+2], threshold);
-			}
-		}
-	return I;
-}
-//YCrCb threshold
-const uchar Y_MIN  = 50;		//0
-const uchar Y_MAX  = 255;		//255
-const uchar Cr_MIN = 133;		//133
-const uchar Cr_MAX = 170;		//173
-const uchar Cb_MIN = 77;		//77
-const uchar Cb_MAX = 127;		//127
-
-Mat skinMat(const Mat& img){
-	Mat channels[3];
-	Mat ret, no_noise;
-	GaussianBlur(img, no_noise, Size2i(5,5), 10, 10, BORDER_DEFAULT);
-	cvtColor(img,ret,cv::COLOR_BGR2YCrCb);
-	inRange(ret,cv::Scalar(Y_MIN,Cr_MIN,Cb_MIN),cv::Scalar(Y_MAX,Cr_MAX,Cb_MAX),ret);
-	//split(apply_effect_rgb(ret,isSkin,0),channels);
+	cv::threshold(ret, ret, 0, maxval, type | THRESH_OTSU);
 
 	return ret;
 }
 
-Mat generarAlien(Mat& skin, Mat& I, int mode) {
-	int nRows = I.rows;
-	int nChannels = I.channels();
-	int nCols = I.cols * nChannels;
-	uchar* p;
-	uchar* s;
-	int j2;
-	if (I.isContinuous()){
-		nCols *= nRows;
-		nRows = 1;
-	}
-	for( int i = 0; i < nRows; ++i)	{
-			p = I.ptr<uchar>(i);
-			s = skin.ptr<uchar>(i);
-			j2 = 0;
-			for ( int j = 0; j < nCols; j = j + nChannels) {
-				switch(mode){
-				case 0:
-					if(s[j2] > 0) alienRed(p[j], p[j+1], p[j+2]);
-					break;
+Mat toBinaryAdapt(Mat &img, int maxval, int type, int method, int blocksize, int C) {
+	Mat ret;
+	cv::cvtColor(img, ret, CV_RGB2GRAY);
+	if (maxval <= 0) cv::max(img, maxval);
 
-				case 1:
-					if(s[j2] > 0) alienGreen(p[j], p[j+1], p[j+2]);
-					break;
+	cv::adaptiveThreshold(ret, ret, maxval, method, type, blocksize, C);
 
-				case 2:
-					if(s[j2] > 0) alienBlue(p[j], p[j+1], p[j+2]);
-					break;
+	return ret;
+}
+
+
+void showHist(Mat &frame) {
+	 //Historiogram
+
+	        Mat channels[3];
+	        split(frame,channels);
+	        float range[] = { 0, (float)histSize } ; //the upper boundary is exclusive
+	        const float* histRange = { range };
+
+	        Mat b_hist, g_hist, r_hist;
+
+	        /// Compute the histograms:
+	        calcHist( &channels[0], 1, 0, Mat(), b_hist, 1, &histSize, &histRange, uniform, accumulat );
+	        calcHist( &channels[1], 1, 0, Mat(), g_hist, 1, &histSize, &histRange, uniform, accumulat );
+	        calcHist( &channels[2], 1, 0, Mat(), r_hist, 1, &histSize, &histRange, uniform, accumulat );
+
+	        // Draw the histograms for R, G and B
+	        int hist_w = 512; int hist_h = 400;
+	        int bin_w = cvRound( (double) hist_w/histSize );
+
+	        Mat histImage( hist_h, hist_w, CV_8UC3, Scalar( 0,0,0) );
+
+	        if (accumulate_hist) {
+	        	for( int i = 1; i < histSize; i++ )
+				{
+					b_hist.at<float>(i) = b_hist.at<float>(i-1) + b_hist.at<float>(i);
+					g_hist.at<float>(i) = g_hist.at<float>(i-1) + g_hist.at<float>(i);
+					r_hist.at<float>(i) = b_hist.at<float>(i-1) + r_hist.at<float>(i);
 				}
-
-				j2++;
 			}
-		}
-	return I;
-}
 
+	        /// Normalize the result to [ 0, histImage.rows ]
+	        normalize(b_hist, b_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+	        normalize(g_hist, g_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
+	        normalize(r_hist, r_hist, 0, histImage.rows, NORM_MINMAX, -1, Mat() );
 
-uchar reducirColorF(uchar &a, uchar* end, double aux[]){
-	int factor = aux[3];
-	return saturate_cast< uchar >(a/factor*factor + factor/2);
-}
+	        /// Draw for each channel
+	        for( int i = 1; i < histSize; i++ )
+	        {
 
-uchar contrastF(uchar &a, uchar* end, double aux[]){
-	double contrast = aux[3];
-	int ret = a * contrast;
-	if (ret > 255) ret = 255;
-	if (ret < 0) ret = 0;
-	return ret;
-}
+	            line( histImage, Point( bin_w*(i-1), hist_h - cvRound(b_hist.at<float>(i-1)) ) ,
+	                             Point( bin_w*(i), hist_h - cvRound(b_hist.at<float>(i)) ),
+	                             Scalar( 255, 0, 0), 2, 8, 0  );
+	            line( histImage, Point( bin_w*(i-1), hist_h - cvRound(g_hist.at<float>(i-1)) ) ,
+	                             Point( bin_w*(i), hist_h - cvRound(g_hist.at<float>(i)) ),
+	                             Scalar( 0, 255, 0), 2, 8, 0  );
+	            line( histImage, Point( bin_w*(i-1), hist_h - cvRound(r_hist.at<float>(i-1)) ) ,
+	                             Point( bin_w*(i), hist_h - cvRound(r_hist.at<float>(i)) ),
+	                             Scalar( 0, 0, 255), 2, 8, 0  );
+	        }
 
-Mat equalizarCV(Mat in) {
-	Mat ycrcb;
-
-	cvtColor(in,ycrcb,CV_BGR2YCrCb);
-
-	vector<Mat> channels;
-	split(ycrcb,channels);
-
-	equalizeHist(channels[0], channels[0]);
-
-	Mat result;
-	merge(channels,ycrcb);
-
-	cvtColor(ycrcb,result,CV_YCrCb2BGR);
-
-	return result;
-}
-
-int countPixels(const cv::Mat &image, cv::Scalar color) {
-
-    Mat binary_image;
-    cv::inRange(image, color, color, binary_image);
-        return cv::countNonZero(binary_image);
-}
-
-Mat equalizarOurs(Mat& in) {
-	Mat result;
-
-	Mat hcv;
-	vector<Mat> channels;
-	cvtColor(in,hcv,CV_BGR2YCrCb);
-	split(hcv,channels);
-
-	float px[255];
-	int size = in.rows * in.cols;
-	for (int i = 0; i < 255; ++i)
-		px[i] = countPixels(channels[0],(Scalar)i);
-
-//	for (int i = 0;i < 256 ; i++) cout << "valuee " << px[i] << endl;
-//	cv::waitKey();
-
-	float cdf[256];
-	cdf[0] = px[0];
-	float min = 255;
-	for (int i = 1; i < 255; ++i) {
-		 cdf[i] = ( cdf[i-1] + px[i] );
-		 if (cdf[i] < min ) min = cdf[i];
-	}
-
-//	for (int i = 0;i < 256 ; i++) cout << "cdfvaluee " << cdf[i] << endl;
-//	cv::waitKey();
-
-	Mat lookUpTable(1, 256, CV_8U);
-	uchar* p = lookUpTable.ptr();
-	for (int i = 0; i < 256; ++i)
-		p[i] = round(((cdf[i] - min ) / (float)(size - min) ) * 255.0f);
-
-//	for (int i = 0;i < 256 ; i++) cout << "valueel " << (int)p[i] << endl;
-//    cv::waitKey();
-
-	LUT(channels[0], lookUpTable,  channels[0]);
-
-	merge(channels,hcv);
-
-	cvtColor(hcv,result, CV_YCrCb2BGR);
-	return result;
-}
-
-void generarDistorsion(Mat& matriz, int mode, float k1) {
-	// https://docs.opencv.org/2.4/modules/imgproc/doc/geometric_transformations.html
-	Mat in = matriz.clone();
-	switch(mode){
-		case 0:
-			k1 = abs(k1); // para cojín
-			break;
-
-		case 1:
-			k1 = -abs(k1); // para cojín
-			break;
-	}
-
-	float k2 = 0.0;
-	float p1 = 0.0;
-	float p2 = 0.0;
-	Mat distCoeffs = Mat(4,1,CV_32FC1);
-	distCoeffs.at<float>(0,0) = k1;
-	distCoeffs.at<float>(1,0) = k2;
-	distCoeffs.at<float>(2,0) = p1;
-	distCoeffs.at<float>(3,0) = p2;
-
-	Mat cam = Mat(3,3,CV_32FC1);
-	cam.at<float>(0,2) = matriz.cols/2;
-	cam.at<float>(1,2) = matriz.rows/2;
-	cam.at<float>(0,0) = matriz.cols;
-	cam.at<float>(1,1) = matriz.rows;
-	cam.at<float>(2,2) = 1;
-
-	undistort(in,matriz,cam,distCoeffs);
+	        namedWindow("calcHist Demo", CV_WINDOW_AUTOSIZE );
+	        imshow("calcHist Demo", histImage );
 }
