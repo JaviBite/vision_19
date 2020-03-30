31ae5e648aec23acf118e997dcd991ebff0195c9
borjaaguadodiez@gmail.com <borjaaguadodiez@gmail.com>
2020-03-30 15:17:38 +0200

    modulo

--- T3/src/main.cpp
+++ T3/src/main.cpp
@@ -32,16 +32,16 @@ int main(int argc, char *argv[]) {
 		cvDestroyWindow("Filtro gaussiano");
 
 		Mat sobelx;
-		Sobel(image, sobelx,CV_32F, 1, 0);
+		Sobel(image, sobelx,CV_32F, 1, 0, 3);
 
 		double minVal, maxVal;
 		minMaxLoc(sobelx, &minVal, &maxVal); //find minimum and maximum intensities
 		cout << "Gradiente vertical " << endl << "minVal : " << minVal << endl << "maxVal : " << maxVal << endl;
 
-		Mat draw;
-		sobelx.convertTo(draw, CV_8U, 255.0/(maxVal - minVal), -minVal * 255.0/(maxVal - minVal));
+		Mat drawx;
+		sobelx.convertTo(drawx, CV_8U, 255.0/(maxVal - minVal), -minVal * 255.0/(maxVal - minVal));
 
-		imshow("Gradiente vertical", draw );                   // Show our image inside it.
+		imshow("Gradiente vertical", drawx );                   // Show our image inside it.
 		waitKey(0);
 		cvDestroyWindow("Gradiente vertical");
 
@@ -52,11 +52,27 @@ int main(int argc, char *argv[]) {
 		minMaxLoc(sobely, &minVal, &maxVal); //find minimum and maximum intensities
 		cout << "Gradiente horizontal (la coordenada y va hacia arriba)" << endl << "minVal : " << minVal << endl << "maxVal : " << maxVal << endl;
 
-		sobely.convertTo(draw, CV_8U, 255.0/(maxVal - minVal), -minVal * 255.0/(maxVal - minVal));
+		Mat drawy;
+		sobely.convertTo(drawy, CV_8U, 255.0/(maxVal - minVal), -minVal * 255.0/(maxVal - minVal));
 
-		imshow("Gradiente horizontal", draw );                   // Show our image inside it.
+		imshow("Gradiente horizontal", drawy );                   // Show our image inside it.
 		waitKey(0);
 		cvDestroyWindow("Gradiente horizontal");
+
+		Mat moduloaux = sobelx;
+		for (int i = 0; i< sobelx.rows; i++)
+			for (int j = 0; j< sobelx.cols; j++)
+				moduloaux.at<float>(i,j) = sqrt(pow(sobelx.at<float>(i,j),2) + pow(sobely.at<float>(i,j),2));
+		minMaxLoc(moduloaux, &minVal, &maxVal); //find minimum and maximum intensities
+		cout << "Modulo" << endl << "minVal : " << minVal << endl << "maxVal : " << maxVal << endl;
+
+		Mat modulo;
+		moduloaux.convertTo(modulo, CV_8U, 255.0/(maxVal - minVal), -minVal * 255.0/(maxVal - minVal));
+
+		imshow("Modulo", modulo );                   // Show our image inside it.
+		waitKey(0);
+		cvDestroyWindow("Modulo");
+
 
 	}
 	else if (strcmp(argv[1], "2") == 0) {
