b84327f25fad84fb2c6175772fb85ce72cb3e138
738866 <738866@unizar.es>
2020-04-28 15:45:33 +0200

    T4 init

---
+++ workspace/T4/src/main.cpp
@@ -0,0 +1,86 @@
+#include <opencv2/opencv.hpp>
+#include "opencv2/core.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/features2d.hpp"
+#include <iostream>
+#include <stdio.h>
+// #include <windows.h> // For Sleep
+#include "utils.hpp"
+#include <fstream>
+#include <string>
+#include <vector>
+// #include <iomanip>
+
+#define HAVE_OPENCV_XFEATURES2D
+
+#ifdef HAVE_OPENCV_XFEATURES2D
+#include "opencv2/xfeatures2d.hpp"
+
+using namespace cv;
+using namespace cv::xfeatures2d;
+
+
+void help() {
+	std::cout << "Usage: " << "program " << "<params>" << std::endl;
+	exit(-1);
+}
+
+int main(int argc, char *argv[]) {
+	if (strcmp(argv[1], "1") == 0) { 	// @suppress("Invalid arguments")
+		std::vector<cv::String> files;
+
+		files.push_back("files/panorama/outdoors_1.jpeg");
+		files.push_back("files/panorama/outdoors_2.jpeg");
+		files.push_back("files/panorama/outdoors_3.jpeg");
+
+		for (cv::String file : files) {
+			Mat src, dst, color_dst;
+			src = cv::imread(file, 0);	// @suppress("Invalid arguments")
+			checkImg(src);
+
+			//cv::cvtColor(src, src, CV_BGR2GRAY);
+
+		    //-- Step 1: Detect the keypoints using SURF Detector
+		    int minHessian = 400;
+		    Ptr<SURF> detector = SURF::create( minHessian );
+		    std::vector<KeyPoint> keypoints;
+		    detector->detect( src, keypoints ); // @suppress("Invalid arguments")
+
+		    //-- Draw keypoints
+		    Mat img_keypoints;
+		    drawKeypoints( src, keypoints, img_keypoints ); // @suppress("Invalid arguments")
+
+
+		    //-- Show detected (drawn) keypoints
+		    imshow("SURF Keypoints", img_keypoints );
+		    waitKey();
+		    return 0;
+		}
+
+
+	}
+	else if (strcmp(argv[1], "2") == 0) { 	// @suppress("Invalid arguments")
+
+		if (argc > 2 && strcmp(argv[2], "horizontal") == 0) { 	 	// @suppress("Invalid arguments")
+
+		}
+
+	}
+	else if (strcmp(argv[1], "3") == 0){	 	// @suppress("Invalid arguments")
+
+	}
+
+	else {
+		help();
+	}
+
+    return 0;
+}
+
+#else
+int main()
+{
+    std::cout << "This program needs the xfeatures2d contrib module to be run." << std::endl;
+    return 0;
+}
+#endif
---
+++ workspace/T4/src/utils.hpp
@@ -0,0 +1,33 @@
+#include <opencv2/opencv.hpp>
+#include <stdio.h>
+#include <functional>
+#include <cmath>
+#include <vector>
+#include <fstream>
+#include <algorithm>
+#include <numeric>
+
+#define CV_BGR2YCrCb COLOR_BGR2YCrCb
+#define CV_YCrCb2BGR COLOR_YCrCb2BGR
+#define CV_RGB2GRAY COLOR_RGB2GRAY
+#define CV_BGR2GRAY COLOR_BGR2GRAY
+#define CV_GRAY2BGR COLOR_GRAY2BGR
+#define CV_BGR2HSV COLOR_BGR2HSV
+#define CV_HSV2BGR COLOR_HSV2BGR
+#define CV_WINDOW_AUTOSIZE WINDOW_AUTOSIZE
+#define CV_TERMCRIT_ITER 1
+#define CV_LOAD_IMAGE_COLOR IMREAD_COLOR
+#define CV_LOAD_IMAGE_GRAYSCALE IMREAD_GRAYSCALE
+#define cvDestroyWindow destroyWindow
+#define CV_RETR_TREE RETR_TREE
+#define CV_CHAIN_APPROX_NONE CHAIN_APPROX_NONE
+
+using namespace cv;
+using namespace std;
+
+void checkImg(Mat img) {
+	if(!img.data or img.empty()) {                          // Check for invalid input
+		cout <<  "Could not open or find the image" << std::endl ;
+		exit(-1);
+	}
+}
